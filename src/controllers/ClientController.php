d9b0d97542b5a6321cd049f089f8bd3487dab015
mtsgmz07 <matissegom@gmail.com>
2023-12-07 11:50:31 +0100

    Modification

--- src/controllers/ClientController.php
+++ src/controllers/ClientController.php
@@ -10,20 +10,20 @@ class ClientController {
         require VIEWS . "gateway/signup.php";
     }
 
-    function signup() {
-        $clientName = $_POST["clientName"];
-        $clientSurname = $_POST["clientSurname"];
-        $clientMail = $_POST["clientMail"];
-        $clientPassword = $_POST["clientPassword"];
-        $clientRetypePassword = $_POST["clientRetypePassword"];
+    // function signup() {
+    //     $clientName = $_POST["clientName"];
+    //     $clientSurname = $_POST["clientSurname"];
+    //     $clientMail = $_POST["clientMail"];
+    //     $clientPassword = $_POST["clientPassword"];
+    //     $clientRetypePassword = $_POST["clientRetypePassword"];
 
-        if ($clientPassword == $clientRetypePassword) {
-            $client = new \ServeEase\models\Client($clientName, $clientSurname, $clientMail, $clientPassword);
-            $client->save();
-            header("Location: /");
-        } else {
-            header("Location: /signup");
-        }
-    }
+    //     if ($clientPassword == $clientRetypePassword) {
+
+    //         $client->save();
+    //         header("Location: /");
+    //     } else {
+    //         header("Location: /signup");
+    //     }
+    // }
 }
 ?>
