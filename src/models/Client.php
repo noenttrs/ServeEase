a52c94bab97fda051be7614d8cdd681b2eabb190
noenttrs <nottarisnoe@gmail.com>
2023-12-07 11:55:01 +0100

    Client modifications

--- src/models/Client.php
+++ src/models/Client.php
@@ -3,5 +3,44 @@
 namespace ServeEase\models;
 
 class Client{
+    private $clientName;
+    private $clientSurname;
+    private $clientMail;
+    private $clientPassword;
+
+    function __construct() {
+    }
+
+    function getClientName() {
+        return $this->clientName;
+    }
+
+    function getClientSurname() {
+        return $this->clientSurname;
+    }
+
+    function getClientMail() {
+        return $this->clientMail;
+    }
+
+    function getClientPassword() {
+        return $this->clientPassword;
+    }
+
+    function setClientName($clientName) {
+        $this->clientName = $clientName;
+    }
+
+    function setClientSurname($clientSurname) {
+        $this->clientSurname = $clientSurname;
+    }
+
+    function setClientMail($clientMail) {
+        $this->clientMail = $clientMail;
+    }
+
+    function setClientPassword($clientPassword) {
+        $this->clientPassword = $clientPassword;
+    }
 
 }
