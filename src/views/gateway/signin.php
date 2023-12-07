b30285917db828974243bc85d76b5e45502d1fd7
mtsgmz07 <matissegom@gmail.com>
2023-12-07 15:11:28 +0100

    edit

--- src/views/gateway/signin.php
+++ src/views/gateway/signin.php
@@ -15,10 +15,12 @@ require_once VIEWS . "base/Header.php" ?>
         <div class="main__signin__content">
             <h2>Connectez vous !</h2>
             <form action="/signin" class="main__signin__content__form" method="post">
+                <div class="main__signin__badRequest">
+                    <p>Les informations fournies, que ce soit l'adresse e-mail ou le mot de passe, sont incorrectes. Veuillez réessayer avec des identifiants valides.</p>
+                </div>
                 <div class="main__signin__clientMail autoInputText">
                     <input type="email" name="clientMail" id="clientMailId" placeholder=" " required>
                     <label for="clientMailId">Adresse e-mail</label>
-                    <small></small>
                 </div>
                 <section class="main__signin__clientPassword">
                     <div class="inputPassword autoInputPassword">
@@ -26,7 +28,6 @@ require_once VIEWS . "base/Header.php" ?>
                         <label for="clientPasswordId">Mot de passe</label>
                         <i class="fa-solid fa-eye-slash"></i>
                     </div>
-                    <small></small>
                 </section>
                 <input type="submit" value="Me connecter" class="main__signin__content__form__submit">
             </form>
