df2ed4a74d218ea1f9f8a7668160c003811b5387
noenttrs <nottarisnoe@gmail.com>
2023-12-07 12:04:06 +0100

    tt

--- src/models/ClientManager.php
+++ src/models/ClientManager.php
@@ -8,14 +8,17 @@ class ClientManager {
     // Add your manager methods here
 
     private $db;
-    
+
     function __construct() {
         $this->db = new \PDO('mysql:host=' . HOST . ';dbname=' . DATABASE, USER, PASSWORD);
     }
 
     function saveClient() {
-
-        $db = new \PDO("mysql:host=localhost;dbname=servease;charset=utf8", "root", "root");
+        $clientName = $client->getClientName();
+        $clientSurname = $client->getClientSurname();
+        $clientMail = $client->getClientMail();
+        $clientPassword = $client->getClientPassword();
+        
         $query = $db->prepare("INSERT INTO client (clientName, clientSurname, clientMail, clientPassword) VALUES (:clientName, :clientSurname, :clientMail, :clientPassword)");
         $query->execute([
             "clientName" => $clientName,
