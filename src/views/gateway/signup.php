3d15f3904f9584f2639f0d0366f11195eed00484
mtsgmz07 <matissegom@gmail.com>
2023-12-07 14:25:22 +0100

    edit

--- src/views/gateway/signup.php
+++ src/views/gateway/signup.php
@@ -18,25 +18,34 @@ require_once VIEWS . "base/Header.php" ?>
                 <div class="main__signup__clientName autoInputText">
                     <input type="text" name="clientName" id="clientNameId" placeholder=" " required>
                     <label for="clientNameId">Prénom</label>
+                    <small></small>
                 </div>
                 <div class="main__signup__clientSurname autoInputText">
                     <input type="text" name="clientSurname" id="clientSurnameId" placeholder=" " required>
                     <label for="clientSurnameId">Nom de famille</label>
+                    <small></small>
                 </div>
                 <div class="main__signup__clientMail autoInputText">
                     <input type="email" name="clientMail" id="clientMailId" placeholder=" " required>
                     <label for="clientMailId">Adresse e-mail</label>
+                    <small></small>
                 </div>
-                <div class="main__signup__clientPassword inputPassword autoInputPassword">
-                    <input type="password" name="clientPassword" id="clientPasswordId" placeholder=" " required>
-                    <label for="clientPasswordId">Mot de passe</label>
-                    <i class="fa-solid fa-eye-slash"></i>
-                </div>
-                <div class="main__signup__clientRetypePassword inputPassword autoInputPassword">
-                    <input type="password" name="clientRetypePassword" id="clientRetypePasswordId" placeholder=" " required>
-                    <label for="clientRetypePasswordId">Confirmer le mot de passe</label>
-                    <i class="fa-solid fa-eye-slash"></i>
-                </div>
+                <section class="main__signup__clientPassword">
+                    <div class="inputPassword autoInputPassword">
+                        <input type="password" name="clientPassword" id="clientPasswordId" placeholder=" " required>
+                        <label for="clientPasswordId">Mot de passe</label>
+                        <i class="fa-solid fa-eye-slash"></i>
+                    </div>
+                    <small></small>
+                </section>
+                <section class="main__signup__clientRetypePassword">
+                    <div class="inputPassword autoInputPassword">
+                        <input type="password" name="clientRetypePassword" id="clientRetypePasswordId" placeholder=" " required>
+                        <label for="clientRetypePasswordId">Confirmer le mot de passe</label>
+                        <i class="fa-solid fa-eye-slash"></i>
+                    </div>
+                    <small></small>
+                </section>
                 <input type="submit" value="Créer mon compte" class="main__signup__content__form__submit">
             </form>
             <p class="main__signup__redirectToSignin">Vous avez déjà un compte ? <a href="/signin">Me connecter</a></p>
