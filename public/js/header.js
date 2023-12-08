5209b20a007d095f179651bed54e174958966822
mtsgmz07 <matissegom@gmail.com>
2023-12-08 14:40:13 +0100

    edit

--- public/js/header.js
+++ public/js/header.js
@@ -1,4 +1,4 @@
-document.querySelector('.body__header__connexion__myAccount__left span')
+document.querySelector('.body__header__connexion__myAccount__left')
     .addEventListener("click", () => {
         document.querySelector(".body__header__connexion__myAccount__rigth i").classList.toggle('active')
         document.querySelector(".body__header__connexion__myAccount__content").classList.toggle('active')
