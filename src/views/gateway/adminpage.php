e52b8b1fee5cacfd6f6f66105b58185600e36210
noenttrs <nottarisnoe@gmail.com>
2023-12-11 15:37:24 +0100

    Admin page form add

--- src/views/gateway/adminpage.php
+++ src/views/gateway/adminpage.php
@@ -14,26 +14,74 @@ require VIEWS . "base/header.php";
         <label for="productName"></label>
         <input type="text" name="productName" id="productName" placeholder="Nom du produit">
     </div>
-    <label for="productDescription"></label>
-    <input type="text" name="productDescription" id="productDescription" placeholder="Description du produit">
-    <label for="productPrice"></label>
-    <input type="number" name="productPrice" id="productPrice" placeholder="Prix du produit">
-    <label for="productStock"></label>
-    <input type="number" name="productStock" id="productStock" placeholder="Stock du produit">
+    <div>
+        <label for="productDescription"></label>
+        <input type="text" name="productDescription" id="productDescription" placeholder="Description du produit">
+    </div>
+    <div>
+        <label for="productPrice"></label>
+        <input type="number" name="productPrice" id="productPrice" placeholder="Prix du produit">
+    </div>
+    <div>
+        <label for="productImage"></label>
+        <input type="file" accept="image/*" name="productImage" id="productImage" placeholder="Image du produit">
+    </div>
 
     <input type="submit" value="Créer le produit">
 </form>
 
 <!-- création d'un menu -->
+<form action="/createMenu" method="post">
+    <div>
+        <label for="menuName"></label>
+        <input type="text" name="menuName" id="menuName" placeholder="Nom du menu">
+    </div>
+    <div>
+        <label for="menuDescription"></label>
+        <input type="text" name="menuDescription" id="menuDescription" placeholder="Description du menu">
+    </div>
+    <div>
+        <label for="menuPrice"></label>
+        <input type="number" name="menuPrice" id="menuPrice" placeholder="Prix du menu">
+    </div>
+    <div>
+        <label for="menuImage"></label>
+        <input type="file" accept="image/*" name="menuImage" id="menuImage" placeholder="Image du menu">
+    </div>
+
+    <input type="submit" value="Créer le menu">
+</form>
 
 
-<!-- surveillance des commandes -->
+<!-- surveillance des commandes -- recherche des commandes -->
+
+<form action="/admin" method="post">
+    <div>
+        <label for="orderSearch"></label>
+        <input type="text" name="orderSearch" id="orderSearch" placeholder="Rechercher une commande">
+    </div>
+
+    <input type="submit" value="Rechercher">
+</form>
 
 
-<!-- surveillance des clients -->
+<!-- surveillance des clients -- recherche de clients -->
+
+<form action="/admin" method="post">
+    <div>
+        <label for="clientSearch"></label>
+        <input type="text" name="clientSearch" id="clientSearch" placeholder="Rechercher un client">
+    </div>
+
+    <input type="submit" value="Rechercher">
+</form>
 
 
-<!-- pannel cuisine -->
+
+
+<!-- pannel cuisine -- redirect to the kitchen's panel -->
+<a href="/kitchen">Pannel cuisine</a>
+
 
 
 <?php require VIEWS . "base/footer.php"; ?> 
