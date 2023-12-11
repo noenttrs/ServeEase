99c5e28997cb9c46a8229b1a8880b732659aa0c3
mtsgmz07 <matissegom@gmail.com>
2023-12-11 16:09:25 +0100

    edit

--- public/js/header.js
+++ public/js/header.js
@@ -1,28 +1,30 @@
-document.querySelector('.body__header__connexion__myAccount__placeholder')
-    .addEventListener("click", () => {
-        document.querySelector(".body__header__connexion__myAccount__rigth i").classList.toggle('active')
-        document.querySelector(".body__header__connexion__myAccount__content").classList.toggle('active')
-    })
-
-document.querySelector('.body__header__menu__up__myAccount__placeholder')
-    .addEventListener("click", () => {
-        document.querySelector(".body__header__menu__up__myAccount__placeholder__rigth i").classList.toggle('active')
-        document.querySelector(".body__header__menu__up__myAccount__content").classList.toggle('active')
-    })
-
 document.addEventListener('DOMContentLoaded', function () {
-    var showPopupButton = document.querySelector('.body__header__nav__menuToggle');
-    var popup = document.querySelector('menu');
+    let showPopupButton = document.querySelector('.body__header__nav__menuToggle');
+    let popup = document.querySelector('menu');
+    let header = document.querySelector('.body__header__connexion__myAccount__placeholder');
+    let icon = document.querySelector(".body__header__connexion__myAccount__rigth i");
+    let content = document.querySelector(".body__header__connexion__myAccount__content");
 
-    // Afficher le popup
-    showPopupButton.addEventListener('click', function () {
-        popup.classList.add("active")
+    header.addEventListener('click', function () {
+        icon.classList.toggle('active');
+        content.classList.toggle('active');
     });
 
-    // Fermer le popup lorsque l'utilisateur clique en dehors de celui-ci
+    document.addEventListener('click', function (event) {
+        if (!header.contains(event.target) && !popup.contains(event.target)) {
+            icon.classList.remove('active');
+            content.classList.remove('active');
+        }
+    });
+
+    showPopupButton.addEventListener('click', function (event) {
+        event.stopPropagation();
+        popup.classList.toggle("active");
+    });
+
     document.addEventListener('click', function (event) {
         if (event.target !== showPopupButton && !popup.contains(event.target)) {
-            popup.classList.remove("active")
+            popup.classList.remove("active");
         }
     });
 });
