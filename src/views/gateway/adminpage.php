7200e478d5fc293bf128347940ac82101c60c9d8
noenttrs <nottarisnoe@gmail.com>
2023-12-11 14:58:06 +0100

    Mise a jour base de donnée

--- src/views/gateway/adminpage.php
+++ src/views/gateway/adminpage.php
@@ -1,18 +1,39 @@
 <?php
 
-if(!isset($_SESSION["client"]) || $_SESSION['client']["CLIENT_ROLE"] == 0) {
+if(!isset($_SESSION["client"]) || $_SESSION['client']["CLIENT_ROLE"] === 0) {
     header("Location: /");
 }
 
-$pathCss = "css/gateway/signin.css";
+$pathCss = "css/main.css";
 require VIEWS . "base/header.php";
 ?>
 
 <!-- création d'un produit -->
+<form action="/createProduct" method="post">
+    <div>
+        <label for="productName"></label>
+        <input type="text" name="productName" id="productName" placeholder="Nom du produit">
+    </div>
+    <label for="productDescription"></label>
+    <input type="text" name="productDescription" id="productDescription" placeholder="Description du produit">
+    <label for="productPrice"></label>
+    <input type="number" name="productPrice" id="productPrice" placeholder="Prix du produit">
+    <label for="productStock"></label>
+    <input type="number" name="productStock" id="productStock" placeholder="Stock du produit">
 
+    <input type="submit" value="Créer le produit">
+</form>
 
 <!-- création d'un menu -->
 
 
+<!-- surveillance des commandes -->
 
-<?php require VIEWS . "base/footer.php"; ?>
+
+<!-- surveillance des clients -->
+
+
+<!-- pannel cuisine -->
+
+
+<?php require VIEWS . "base/footer.php"; ?> 
