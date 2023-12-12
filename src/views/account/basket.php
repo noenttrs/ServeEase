af6f144f29956ff039ae8edc27ef49969714e117
mtsgmz07 <matissegom@gmail.com>
2023-12-12 15:54:46 +0100

    edit

--- src/views/account/basket.php
+++ src/views/account/basket.php
@@ -55,10 +55,71 @@ require_once VIEWS . "base/Header.php"; ?>
                     </a>
                 </td>
             </tr>
+            <tr class="body__main__product__table__descriptionOfProduct">
+                <td class="body__main__product__table__descriptionOfProduct__imageOfProduct">
+                    <div class="body__main__product__table__descriptionOfProduct__imageOfProduct__container">
+                        <img src="img/BO_MCFIRST.webp" alt="">
+                    </div>
+                </td>
+                <td class="body__main__product__table__descriptionOfProduct__nameOfProduct">
+                    test
+                </td>
+                <td class="body__main__product__table__descriptionOfProduct__quantityOfProduct">
+                    <div class="body__main__product__table__descriptionOfProduct__quantityOfProduct__container">
+                        <i class="fa-solid fa-minus body__main__product__table__descriptionOfProduct__quantityOfProduct__container__minus"></i>
+                        <span>1</span>
+                        <input type="hidden" value="">
+                        <i class="fa-solid fa-plus body__main__product__table__descriptionOfProduct__quantityOfProduct__container__plus"></i>
+                    </div>
+                </td>
+                <td class="body__main__product__table__descriptionOfProduct__priceOfProduct">
+                    €120 EUR
+                </td>
+                <td class="body__main__product__table__descriptionOfProduct__deleteProduct">
+                    <a href="/basket?delete=" class="">
+                        <i class="fa-solid fa-xmark"></i>
+                    </a>
+                </td>
+            </tr>
+            <tr class="body__main__product__table__descriptionOfProduct">
+                <td class="body__main__product__table__descriptionOfProduct__imageOfProduct">
+                    <div class="body__main__product__table__descriptionOfProduct__imageOfProduct__container">
+                        <img src="img/BO_MCFIRST.webp" alt="">
+                    </div>
+                </td>
+                <td class="body__main__product__table__descriptionOfProduct__nameOfProduct">
+                    test
+                </td>
+                <td class="body__main__product__table__descriptionOfProduct__quantityOfProduct">
+                    <div class="body__main__product__table__descriptionOfProduct__quantityOfProduct__container">
+                        <i class="fa-solid fa-minus body__main__product__table__descriptionOfProduct__quantityOfProduct__container__minus"></i>
+                        <span>1</span>
+                        <input type="hidden" value="">
+                        <i class="fa-solid fa-plus body__main__product__table__descriptionOfProduct__quantityOfProduct__container__plus"></i>
+                    </div>
+                </td>
+                <td class="body__main__product__table__descriptionOfProduct__priceOfProduct">
+                    €120 EUR
+                </td>
+                <td class="body__main__product__table__descriptionOfProduct__deleteProduct">
+                    <a href="/basket?delete=" class="">
+                        <i class="fa-solid fa-xmark"></i>
+                    </a>
+                </td>
+            </tr>
         </table>
     </section>
     <section class="body__main__checkout">
-        
+        <div class="body__main__checkout__backToShop">
+            <i class="fa-solid fa-arrow-left"></i>
+            <a href="">Poursuivre mes achats</a>
+        </div>
+        <div class="body__main__checkout__stripe">
+            <p>Total:<br>€120 EUR</p>
+            <form action="" class="body__main__checkout__stripe__form">
+                <input type="submit" value="Procéder au paiement">
+            </form>
+        </div>
     </section>
 </main>
 
