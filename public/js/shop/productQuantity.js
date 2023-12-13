51733dbe5b8f0915f9aaac8471612154edda3616
mtsgmz07 <matissegom@gmail.com>
2023-12-13 14:28:44 +0100

    edit

--- public/js/shop/productQuantity.js
+++ public/js/shop/productQuantity.js
@@ -1,16 +1,19 @@
 document.querySelector(".body__main__presentation__content__addPanerForm__inputQuantity__minus")
     .addEventListener('click', () => {
         let placeholder = document.querySelector(".body__main__presentation__content__addPanerForm__inputQuantity__placeholder")
+        let productQuantityHidden = document.querySelector(".body__main__presentation__content__addPanerForm__inputQuantity__productQuantity")
         if (isNaN(Number(placeholder.innerText))) return placeholder.innerText = 1
         if (Number(placeholder.innerText) <= 1) return
+        productQuantityHidden.value = Number(placeholder.innerText) - 1
         placeholder.innerText = Number(placeholder.innerText) - 1
     })
 
 document.querySelector(".body__main__presentation__content__addPanerForm__inputQuantity__plus")
     .addEventListener('click', () => {
         let placeholder = document.querySelector(".body__main__presentation__content__addPanerForm__inputQuantity__placeholder")
+        let productQuantityHidden = document.querySelector(".body__main__presentation__content__addPanerForm__inputQuantity__productQuantity")
         if (isNaN(Number(placeholder.innerText))) return placeholder.innerText = 1
         if (Number(placeholder.innerText) < 1) return
+        productQuantityHidden.value = Number(placeholder.innerText) + 1
         placeholder.innerText = Number(placeholder.innerText) + 1
-
     })
